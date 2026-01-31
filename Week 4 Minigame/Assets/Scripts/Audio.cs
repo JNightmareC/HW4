8b28482e11e2681a841888d54e952ea9b502e3b8
Jasmine Caicedo <jecaiced@uci.edu>
2026-01-30 20:33:24 -0800

    Completed audio script

--- Week 4 Minigame/Assets/Scripts/Audio.cs
+++ Week 4 Minigame/Assets/Scripts/Audio.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
+    [SerializeField] private AudioSource _fly;
+    [SerializeField] private AudioSource _getPoint;
+    [SerializeField] private AudioSource _die;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController.Instance._birdPlayer._flapped += Fly;
+        GameController.Instance._birdPlayer._hitPipe += Die;
+        GameController.Instance._birdPlayer._points += GetPoints;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Fly()
     {
-        
+       _fly.Play(); 
+    }
+
+    private void GetPoints()
+    {
+        _getPoint.Play(); 
+ 
+    }
+
+    private void Die()
+    {
+        _die.Play(); 
+  
     }
 }
