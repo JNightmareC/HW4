e33797f766986bc0cad87fcceb8d2615cacd97c5
Jasmine Caicedo <jecaiced@uci.edu>
2026-01-30 20:32:58 -0800

    Completed UI script

--- Week 4 Minigame/Assets/Scripts/UiPoints.cs
+++ Week 4 Minigame/Assets/Scripts/UiPoints.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UiPoints : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _PointsGainedText;
+    private int _startPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController.Instance._birdPlayer._points += PointsGained;
+        _startPoints = 0;
+        _PointsGainedText.text = _startPoints.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PointsGained()
     {
-        
+        Debug.Log("am in points thing");
+        _startPoints += 1;
+        _PointsGainedText.text = _startPoints.ToString();
     }
+    
+
+    
 }
